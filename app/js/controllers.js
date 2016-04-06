bf002b84ac14ab097644ebea0c6d336c7a91bbaa
Teisia Park <teisia@gmail.com>
2016-04-05 20:31:29 -0600

    successfully calling trip service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,14 @@
-app.controller('MainController', ['$scope', '$http', function($scope, $http){
-     $scope.test='HTML and Routes Working';
+app.controller('MainController', ['$scope', '$http', 'TripService', function($scope, $http, TripService){
 
      $scope.togglePostForm = function () {
        $scope.showme = !$scope.showme;
-     }
+     };
+
+     TripService.getTrips().then(function(payload){
+      $scope.trip_collection = payload.data;
+    }, function(error){
+      console.log("an error occurred");
+    });
 
 }])
 
--- app/js/services.js
+++ app/js/services.js
@@ -1,19 +1,9 @@
 app.service("TripService", function($http){
-  var CatService = {};
+  var TripService = {};
 
-  TripService.getCats = function(){
-    return $http.get("http://localhost:3000/trips", {method: "jsponp"});
-    // return $http.get("https://frozen-badlands-34577.herokuapp.com/cats", {method: "jsonp"});
+  TripService.getTrips = function(){
+    //return $http.get("http://localhost:3000/trips", {method: "jsonp"});
+    return $http.get("https://teisia-capstone-api.herokuapp.com/trips", {method: "jsonp"});
   }
-
-  TripService.getCat = function(cat_id){
-    return $http.get("http://localhost:3000/cats/"+cat_id);
-    // return $http.get("https://frozen-badlands-34577.herokuapp.com/cats/"+cat_id, {method: "jsonp"});
-  }
-
-  TripService.newCat = function(cat_object) {
-    return $http.post("http://localhost:3000/cats/", cat_object);
-  }
-
-  return CatService;
+  return TripService;
 });
